78a375ef446cc951bd194ec60bc4db77843c034c
Hassansalam4 <atuman4@gmail.com>
2024-12-17 14:00:52 -0500

    working on footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Footer from './Components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
