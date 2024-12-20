ccee6777792d1e7ef7b7d3d3cc3d62970246bcf6
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2024-12-19 23:00:06 -0600

    Bug fixes related to Spotify

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { signup } from '../api/authAPI';
 
 const SignUp: React.FC = () => {
   const [formData, setFormData] = useState({
