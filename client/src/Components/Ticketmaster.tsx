f9c5f0fb932f7ab5a6f7b5866ad8a700bffcb7cc
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2024-12-19 22:25:36 -0600

    new ticketmaster feature implemented

--- client/src/Components/Ticketmaster.tsx
+++ client/src/Components/Ticketmaster.tsx
@@ -14,16 +14,44 @@ const TicketMasterList: React.FC<TicketMasterListProps> = ({ ticketmasters }) =>
             <h2 className="pb-5">
                 Here are the events!
             </h2>
-            {ticketmasters && ticketmasters.map((ticketmaster) => (
+            {/* {ticketmasters && ticketmasters.map((ticketmaster) => (
                 <div className="row align-center mb-5" key={ticketmaster.name}>
                     <div className="col-md-6">
-                        <h3>{ticketmaster.startDateTime}. {ticketmaster.endDateTime}</h3>
+                        <h3>{ticketmaster.sales.public.startDateTime}. {ticketmaster.sales.public.endDateTime}</h3>
                     </div>
                     <div className="col-md-6">
                         <h4><a href={`${ticketmaster.url}`}>{ticketmaster.url}</a></h4>
                     </div>
                 </div>
-            ))}
+            ))} */}
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Start Date Time</th>
+                        <th scope="col">End Date Time</th>
+                        <th scope="col">Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* <tr>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr> */}
+                    {ticketmasters && ticketmasters.map((ticketmaster) => (
+                        <tr>
+                            
+                                <td>{ticketmaster.sales.public.startDateTime}</td>
+                                <td>{ticketmaster.sales.public.endDateTime}</td>
+                            
+                                <td><a href={`${ticketmaster.url}`} target='_blank'>Click to Purchase</a></td>
+                                </tr>  
+                       
+                    ))}
+
+                </tbody>
+            </table>
+
         </>
     );
 };
