9d9764998e9155edef73b8d189a66966181602b0
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2024-12-20 18:46:50 -0600

    Changed Home Page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,17 @@ import React from 'react';
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Tune Sphere</h1>
+      <h3>Welcome</h3>
+      <img src="/tuneSphere-no-bg.png" alt="Mag picture" width="150" height="150"></img>
+      <p>
+        Dive into the ultimate platform for music lovers, where discovering your favorite artists and finding live events has never been easier. Search for music artists to explore their complete discographies, learn about their journeys, and stay updated on their latest releases. Whether you're a fan of pop, rock, jazz, or any genre in between, you can uncover detailed profiles of musicians and bands tailored to your preferences.
+      </p>
+      <p>
+        Planning to catch a live performance? Effortlessly search for upcoming events through our Ticketmaster integration. Browse concerts, festivals, and special performances happening near you or in your favorite cities. With real-time event updates and ticket availability, you'll never miss the chance to see your favorite artists live.7
+      </p>
+      <p>
+        From discovering hidden gems to tracking big-name stars, our platform makes it easy to connect with the music world. Create personalized watchlists, set reminders for events, and share your finds with friends. Start your journey today and experience music like never before!
+      </p>
     </div>
   );
 };
